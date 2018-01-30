38cee7b488eb505a1ffb90a7ac74f52f3390646f
MadLicorice <madlicorice101@gmail.com>
2018-01-29 19:49:08 -0800

    Added youtube api.

--- src/config/youtube.example.js
+++ src/config/youtube.example.js
@@ -1,2 +1,2 @@
-// Put your YouTube API keys here!
-window.YOUTUBE_API_KEY = 'YOUR_API_KEY_HERE';
+// Put your YouTube API keys here! /* AIzaSyDDww_2yI8RQcCqF9mZl_WdNP_sxIkc6mk */
+window.YOUTUBE_API_KEY = 'AIzaSyDDww_2yI8RQcCqF9mZl_WdNP_sxIkc6mk';
